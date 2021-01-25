05d8aad9fb11281967cb4647ad6c125d946043a6
goldsilver12 <62609397+goldsilver12@users.noreply.github.com>
2021-01-25 15:44:03 +0200

    FirstCommit

---
+++ alevel-HW12/src/main/java/ua/com/alevel/Demo.java
@@ -0,0 +1,40 @@
+package ua.com.alevel;
+
+import java.io.*;
+
+
+public class Demo {
+    public static String read(String path) {
+        StringBuilder stringBuilder;
+        stringBuilder = new StringBuilder();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String currentString;
+            while ((currentString = reader.readLine()) != null) {
+                stringBuilder.append(currentString);
+                stringBuilder.append(" \n ");
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException " + e.getMessage());
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        File testFile = new File("C:\\test1.txt");
+        // Once we have changed the name of ours file or  the directory we get the Exception.
+        try {
+            System.out.println(read("C:\\test1.txt"));
+        } finally {
+            if (testFile.exists()) {
+                System.out.println("Program implements correctly, we have file to work with.");
+            } else if (!testFile.exists()) {
+                System.out.println("There is no file to work with.");
+            }
+        }
+    }
+}
